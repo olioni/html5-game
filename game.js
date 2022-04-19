48282c12fedc148fb0a4932815816739e6de2103
Ian Tairea <waipuna@gmail.com>
2022-04-19 22:13:27 +1200

    updated map

--- game.js
+++ game.js
@@ -865,8 +865,8 @@ class GamePlay extends Phaser.Scene {
     // ====================== Tiled JSON map ===========================
 
     // OLIONI'S MAP
-    // this.load.tilemapTiledJSON("map", "https://cdn.glitch.global/6ec21438-e8d9-4bed-8695-1a8695773d71/olioni-map-3.json?v=1650322562271")
-    this.load.tilemapTiledJSON("map", "../map/olioni-map-3.json");
+    this.load.tilemapTiledJSON("map", "https://cdn.glitch.global/6ec21438-e8d9-4bed-8695-1a8695773d71/olioni-map-3.json?v=1650363170751")
+    // this.load.tilemapTiledJSON("map", "../map/olioni-map-3.json");
 
     // ====================== Sound effects ===========================
     this.load.audio(
